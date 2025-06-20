fa2237d4019771dd151085ea6022cd4b33035693
Tomás García Gobet <t@gobet.ar>
2025-06-20 11:52:04 +0200

    adds fluent icons

--- background.js
+++ background.js
@@ -4,31 +4,24 @@ chrome.action.onClicked.addListener(async (tab) => {
       target: { tabId: tab.id },
       func: () => {
         if (navigator.share) {
-          navigator
-            .share({
-              title: document.title,
-              url: window.location.href,
-            })
-            .catch((error) => {
-              if (error.name === "AbortError") {
-                return;
-              }
+          navigator.share({ url: window.location.href }).catch((error) => {
+            if (error.name === "AbortError") {
+              return;
+            }
 
-              if (navigator.clipboard) {
-                navigator.clipboard
-                  .writeText(window.location.href)
-                  .then(() => {
-                    alert(
-                      "Sharing failed, but URL has been copied to clipboard"
-                    );
-                  })
-                  .catch(() => {
-                    alert("Sharing failed and unable to copy to clipboard");
-                  });
-              } else {
-                alert("Sharing failed and clipboard not available");
-              }
-            });
+            if (navigator.clipboard) {
+              navigator.clipboard
+                .writeText(window.location.href)
+                .then(() => {
+                  alert("Sharing failed, but URL has been copied to clipboard");
+                })
+                .catch(() => {
+                  alert("Sharing failed and unable to copy to clipboard");
+                });
+            } else {
+              alert("Sharing failed and clipboard not available");
+            }
+          });
         } else {
           if (navigator.clipboard) {
             navigator.clipboard
